6d90f45179585d4168f6b4d68d512d3c3315a451
Kelvin Zhang <me@kelvinzhang.ca>
2020-05-31 13:18:47 -0700

    Add OSM

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import ReactDOM from "react-dom";
+import mapboxgl from "mapbox-gl";
+import logo from "./logo.svg";
+import { accessToken } from "./creds";
+import "./App.css";
+
+mapboxgl.accessToken = accessToken;
 
 function App() {
+  const [lng, setLng] = useState(5);
+  const [lat, setLat] = useState(34);
+  const [zoom, setZoom] = useState(2);
+
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mapContainer" ref={mapContainer}></div>
     </div>
   );
 }
