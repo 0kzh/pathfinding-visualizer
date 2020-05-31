24895dd3fe8140b555c8bfec0501893d9d73bd13
Kelvin Zhang <me@kelvinzhang.ca>
2020-05-31 13:55:36 -0700

    Switch to Leaflet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 import mapboxgl from "mapbox-gl";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import logo from "./logo.svg";
 import { accessToken } from "./creds";
 import "./App.css";
@@ -8,23 +9,18 @@ import "./App.css";
 mapboxgl.accessToken = accessToken;
 
 function App() {
-  const [lng, setLng] = useState(5);
-  const [lat, setLat] = useState(34);
-  const [zoom, setZoom] = useState(2);
+  const [lng, setLng] = useState(-122.4372);
+  const [lat, setLat] = useState(37.7546);
+  const [zoom, setZoom] = useState(11.48);
 
-  const mapContainer = useRef(null);
-
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-  });
   return (
     <div className="App">
-      <div className="mapContainer" ref={mapContainer}></div>
+      <Map center={[lat, lng]} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </Map>
     </div>
   );
 }
