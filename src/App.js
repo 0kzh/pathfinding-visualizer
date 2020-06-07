c6fc99c719bb72fe2fd0da816017c2b182f444f4
Kelvin Zhang <me@kelvinzhang.ca>
2020-06-07 11:10:59 -0700

    Node data from SF

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
 import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
-import mapboxgl from "mapbox-gl";
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import logo from "./logo.svg";
+import { Icon } from "leaflet";
+import { Map, Marker, Popup, TileLayer, ZoomControl } from "react-leaflet";
+import nodeData from "./data/sanfran.json";
+import locations from "./data/locations.json";
 import { accessToken } from "./creds";
+import Settings from "./components/Settings";
 import "./App.css";
-
-mapboxgl.accessToken = accessToken;
 
 function App() {
   const [lng, setLng] = useState(-122.4372);
   const [lat, setLat] = useState(37.7546);
   const [zoom, setZoom] = useState(11.48);
 
+  const icon = new Icon({
+    iconUrl: "/marker.svg",
+    iconSize: [25, 25],
+  });
+
+  // console.log(nodeData["6597683646"]);
   return (
     <div className="App">
-      <Map center={[lat, lng]} zoom={zoom}>
+      <Settings />
+      <Map center={[lat, lng]} zoom={zoom} zoomControl={false}>
         <TileLayer
           url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+
+        <ZoomControl position={"bottomleft"} />
+
+        {/* <Marker
+          position={[, val.lat]}
+          onClick={() => {
+            // setActivePark(park);
+          }}
+          icon={icon}
+        /> */}
+        {/* {Object.keys(nodeData).map((key, index) => {
+          const val = nodeData[key];
+
+          if (index % 50 == 0 && val) {
+            return (
+              <Marker
+                key={key}
+                position={[val.lat, val.lon]}
+                onClick={() => {
+                  console.log(key);
+                }}
+                icon={icon}
+              />
+            );
+          } else {
+            return null;
+          }
+        })} */}
+        {locations["san_francisco"].map((loc) => {
+          const val = nodeData[loc.node];
+          console.log(val);
+          return (
+            <Marker
+              key={loc.node}
+              position={[val.lat, val.lon]}
+              onClick={() => {
+                console.log(loc.node);
+              }}
+              icon={icon}
+            />
+          );
+        })}
       </Map>
     </div>
   );
 }
-
 export default App;
